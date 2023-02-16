8144302d89e5f22beb1481fd8947b3c7fb16463c
KichZero <kichmg22@gmail.com>
2023-02-16 02:30:55 +0200

    first keyframe command

---
+++ js/shopping-list.js
@@ -0,0 +1,20 @@
+$('ul').on('click', 'li', function() {
+	$(this).toggleClass('done');
+})
+
+$('ul').on('click', 'span', function(event) {
+	event.stopPropagation();
+	$(this).parent().fadeOut(function() {
+		$(this).remove();
+	});
+})
+
+$('input').keypress(function(event) {
+	if(event.which === 13) {
+
+		var itemText = $(this).val();
+		$(this).val('');
+		$('ul').append('<li>' + itemText +
+			'<span><i class="fas fa-backspace"></i></span></li>');
+	}
+})
